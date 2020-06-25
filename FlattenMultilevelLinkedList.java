ccae2dbf9754194a359c38d8a48155bc83499a61
kukkisandhu <gursharansandhu1991@gmail.com>
2020-06-24 20:15:35 -0400

    Update FlattenMultilevelLinkedList.java

--- FlattenMultilevelLinkedList.java
+++ FlattenMultilevelLinkedList.java
@@ -34,3 +34,49 @@ class Solution {
         return result;
     }
 }
+
+
+/*
+ITERATIVE SOLUTION 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+/*
+class Solution {
+    Node current;
+    public Node flatten(Node head) {        
+        if(head==null){
+            return null;
+        }
+        Stack<Node> mystack=new Stack<>();
+        mystack.push(head);
+        current=new Node(0,null,null,null);
+        Node result=current;
+        while(!mystack.empty()){
+            Node mynode=mystack.pop();
+            current.val=mynode.val;
+            current.next=new Node(0,current,null,null);
+            current=current.next;
+            if(mynode.next!=null){
+                mystack.push(mynode.next);
+            }
+            if(mynode.child!=null){
+                mystack.push(mynode.child);
+            }
+            
+        }
+        current.prev.next=null;
+        return result;
+        
+    }
+}
+
+
+
+*/

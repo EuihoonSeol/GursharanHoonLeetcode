fbd71cfe4884f49bd88552953d2790ce13cd407c
kukkisandhu <gursharansandhu1991@gmail.com>
2020-08-07 19:19:54 -0400

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        if(x<=1){
+            return x;
+        }
+        long start=1;
+        long end=x;
+        while(start+1<end){
+            long mid=(start+(end-start)/2);
+            if(mid*mid==x){
+                return (int)mid;
+            }
+           if(mid*mid<x){
+               start=mid;
+           }
+            else{
+                end=mid;
+            }
+        }
+        
+        if(end*end==x){
+            return (int)(end);
+        }
+        else{
+            return (int)(start);
+        }
+        
+        
+    }
+}
